023e9c7e3599652f3708a5352df4f1fcbce428ab
malcolm <malcolm19880513@gmail.com>
2026-01-15 11:44:22 +0900

    daily

--- pkg/new_rb_tree.py
+++ pkg/new_rb_tree.py
@@ -1,4 +1,3 @@
-import random
 class RBNode:
     def __init__(self, val):
         self.red = False
@@ -9,103 +8,141 @@ class RBNode:
 
 
 class RBTree:
+    # 创建一个nil节点
     def __init__(self):
         self.nil = RBNode(None)
         self.nil.red = False
         self.nil.left = None
         self.nil.right = None
         self.root = self.nil
-    # 向左旋转,
-    # 拿出该节点的右节点
-    def rotate_left(self, pivot_parent:RBNode):
-        # 如果当前节点是最外面节点, 停止
-        if pivot_parent is self.nil:
-            return
-        # 如果当前节点的右节点, 是最外面节点, 停止
-        if pivot_parent.right is self.nil:
-            return
-        # 拿出最右面节点当pivot
-        pivot = pivot_parent.right
-        # 老parent 指向 pivot 的左节点
-        pivot_parent.right = pivot.left
-        # 如果pivot 的旧 左节点, 不是nil
-        # 更新向上指针
-        if pivot.left is not self.nil:
-            pivot.left.parent = pivot_parent
-        # 更新pivot 的向上指针,
-        pivot.parent = pivot_parent.parent
-
-        ## 更新向下指针
-        # 如果老pivot_parent
-        if pivot_parent is self.root:
-            self.root = pivot
-        elif pivot_parent is pivot_parent.parent.left:
-            pivot_parent.parent.left = pivot
-        elif pivot_parent is pivot_parent.parent.right:
-            pivot_parent.parent.right = pivot
-        # 更新pivot 的向下指针    
-        pivot.left = pivot_parent
-        # 更新向上指针
-        pivot_parent.parent = pivot
-
-
-    def rotate_right(self, pivot_parent: RBNode):
-        if pivot_parent is self.nil:
-            return
-        if pivot_parent.left is self.nil:
-            return
-
-        pivot = pivot_parent.left
-
-        # x 接管 y.right
-        pivot_parent.left = pivot.right
-        if pivot.right is not self.nil:
-            pivot.right.parent = pivot_parent
-
-        # y 接管 x 的父节点
-        pivot.parent = pivot_parent.parent
-        if pivot_parent is self.root:
-            self.root = pivot
-        elif pivot_parent is pivot_parent.parent.left:
-            pivot_parent.parent.left = pivot
-        else:
-            pivot_parent.parent.right = pivot
-
-        # 完成旋转
-        pivot.right = pivot_parent
-        pivot_parent.parent = pivot
-
 
     def insert(self, val):
-        # 1. 已val创建一个节点,并默认它为最外面的叶子节点,
-        #  所以将它的左右为nil
+        # 新建一个红节点, 没有任何父子关系
         new_node = RBNode(val)
-        new_node.red = True
+        new_node.parent = None
         new_node.left = self.nil
         new_node.right = self.nil
-        new_node.parent = None
+        new_node.red = True
 
-        # 2. Find parent
         parent = None
         current = self.root
-        # 如果没有到最外面的nil, 找到合适的插入点
-        while current is not self.nil:
+        # 找到符合BST规则,的插入位置
+        # 此节点是最下方节点, 左右节点必然nil
+        while current != self.nil:
             parent = current
             if new_node.val < current.val:
                 current = current.left
             elif new_node.val > current.val:
                 current = current.right
             else:
-                # Duplicate value
+                # duplicate, just ignore
                 return
-
-        # 3.Insert new_node, 子节点指向合适的节点
+        # 新节点向上建立联系
         new_node.parent = parent
-        # 4. 父节点向下建立联系
+        # 更新父节点的向下联系
         if parent is None:
-            # Tree was empty
             self.root = new_node
         elif new_node.val < parent.val:
             parent.left = new_node
         else:
             parent.right = new_node
+        
+        # ?
+        self.fix_insert(new_node)
+
+    def fix_insert(self, new_node:RBNode):
+        current_node = new_node
+        # 如果不是根节点, 并且父亲为红
+        # 那么它不能为红
+        while (current_node is not self.root 
+               and current_node.parent.red):
+            # 父亲 爷爷 和uncle
+            parent = current_node.parent
+            grandparent = parent.parent
+            # 如果父亲是右节点
+            if(parent is grandparent.right):
+                # uncle 是左
+                uncle = grandparent.left
+                # 如果uncle 红,
+                if  uncle.red:
+                    # 全部涂黑
+                    uncle.red = False
+                    parent.red = False
+                    # 祖父为红
+                    grandparent.red = True
+                    current_node = grandparent
+
+
+                else:
+                    if current_node is parent.left:
+                        # Move up the tree by making the current node the parent ?
+                        current_node = parent
+                        self.rotate_right(current_node)
+                    parent = current_node.parent
+                    parent.red = False
+                    grandparent.red = True
+                    self.rotate_right(grandparent)
+
+            else:
+                uncle = grandparent.right
+                if uncle.red:
+                    uncle.red = False
+                    parent.red = False
+                    grandparent.red = True
+                    # Move up the tree by making the current node the grandparent?
+                    current_node = grandparent
+                else:
+                    if current_node is parent.right:
+                        current_node = parent
+                        self.rotate_left(current_node)
+                    parent = current_node.parent
+                    parent.red = False
+                    grandparent.red = True
+                    self.rotate_right(grandparent)
+        self.root.red = False
+
+
+    def exists(self, val):
+        curr = self.root
+        while curr != self.nil and val != curr.val:
+            if val < curr.val:
+                curr = curr.left
+            else:
+                curr = curr.right
+        return curr
+
+    def rotate_left(self, pivot_parent):
+        if pivot_parent == self.nil or pivot_parent.right == self.nil:
+            return
+        pivot = pivot_parent.right
+        pivot_parent.right = pivot.left
+        if pivot.left != self.nil:
+            pivot.left.parent = pivot_parent
+
+        pivot.parent = pivot_parent.parent
+        if pivot_parent.parent is None:
+            self.root = pivot
+        elif pivot_parent == pivot_parent.parent.left:
+            pivot_parent.parent.left = pivot
+        else:
+            pivot_parent.parent.right = pivot
+        pivot.left = pivot_parent
+        pivot_parent.parent = pivot
+
+    def rotate_right(self, pivot_parent):
+        if pivot_parent == self.nil or pivot_parent.left == self.nil:
+            return
+        pivot = pivot_parent.left
+        pivot_parent.left = pivot.right
+        if pivot.right != self.nil:
+            pivot.right.parent = pivot_parent
+
+        pivot.parent = pivot_parent.parent
+        if pivot_parent.parent is None:
+            self.root = pivot
+        elif pivot_parent == pivot_parent.parent.right:
+            pivot_parent.parent.right = pivot
+        else:
+            pivot_parent.parent.left = pivot
+        pivot.right = pivot_parent
+        pivot_parent.parent = pivot
